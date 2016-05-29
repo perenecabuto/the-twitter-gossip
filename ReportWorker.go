657f9d15ddbb00d5bc8e2fe061c4acc542322a64
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-05-29 03:35:18 -0300

    Avoid race condition

--- ReportWorker.go
+++ ReportWorker.go
@@ -19,31 +19,38 @@ type ReportWorker struct {
 	Interval    time.Duration
 	events      EventGroup
 	OnTimeEvent func(t time.Time, events EventGroup)
+	reportChann chan string
 }
 
 func NewReportWorker(interval time.Duration) *ReportWorker {
-	return &ReportWorker{interval, EventGroup{}, nil}
+	return &ReportWorker{interval, EventGroup{}, nil, make(chan string)}
 }
 
 func (rw *ReportWorker) Start() {
 	ticker := time.NewTicker(rw.Interval)
-	for t := range ticker.C {
-		fmt.Println(t, rw.OnTimeEvent != nil)
-		if rw.OnTimeEvent != nil {
-			go rw.OnTimeEvent(t, rw.events.Clone())
-		}
+	for {
+		select {
+		case t := <-ticker.C:
+			fmt.Println(t, rw.OnTimeEvent != nil)
+			if rw.OnTimeEvent != nil {
+				go rw.OnTimeEvent(t, rw.events.Clone())
+			}
 
-		for key, _ := range rw.events {
-			rw.events[key] = 0
+			for key, _ := range rw.events {
+				rw.events[key] = 0
+			}
+		case name := <-rw.reportChann:
+			val, ok := rw.events[name]
+			if !ok {
+				val = 0
+			}
+
+			rw.events[name] = val + 1
+			fmt.Println("Event", name, rw.events[name])
 		}
 	}
 }
 
 func (rw *ReportWorker) ReportEvent(name string) {
-	val, ok := rw.events[name]
-	if !ok {
-		val = 0
-	}
-
-	rw.events[name] = val + 1
+	rw.reportChann <- name
 }
