8f7581eeca102f783ad2967e41d0da04c9a9735d
Felipe Ramos (Cabuto) <felipe.ramos@corp.globo.com>
2016-05-31 23:32:12 -0300

    Implement route to start/stop workers

--- handler.go
+++ handler.go
@@ -29,42 +29,56 @@ type GossipListPayload struct {
 
 type GossipResourceHandler struct {
 	service GossipService
+	pool    *GossipWorkerPool
 }
 
-func (gah *GossipResourceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func NewGossipResourceHandler(s GossipService, p *GossipWorkerPool) *GossipResourceHandler {
+	return &GossipResourceHandler{s, p}
+}
+
+func (h *GossipResourceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	splittedPath := strings.Split(r.URL.Path, "/")
-	gossip := strings.TrimSpace(splittedPath[len(splittedPath)-1])
+	gossip := splittedPath[2]
+	action := ""
+	if len(splittedPath) > 3 {
+		action = splittedPath[3]
+	}
+
+	log.Println("HANDLE", r.URL.Path, r.Method, splittedPath, gossip, action)
 	w.Header().Set("Content-Type", "application/json")
 	if r.Method == "GET" {
 		if len(gossip) == 0 {
-			gah.List(w, r)
+			h.List(w, r)
+		} else if action == "start" {
+			h.StartWorker(gossip, w, r)
+		} else if action == "stop" {
+			h.StopWorker(gossip, w, r)
 		} else {
-			gah.Get(gossip, w, r)
+			h.Get(gossip, w, r)
 		}
 	} else if r.Method == "POST" {
-		gah.Create(w, r)
+		h.Create(w, r)
 	}
 }
 
-func (gah *GossipResourceHandler) List(w http.ResponseWriter, r *http.Request) {
-	log.Println("list")
+func (h *GossipResourceHandler) List(w http.ResponseWriter, r *http.Request) {
 	payload := &GossipListPayload{[]*GossipPayload{}}
-	gossips, err := gah.service.FindAllGossip()
+	gossips, err := h.service.FindAllGossip()
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), 500)
 		return
 	}
 	for _, g := range gossips {
-		payload.Gossips = append(payload.Gossips, gah.buildGossipPayload(g))
+		payload.Gossips = append(payload.Gossips, h.buildGossipPayload(g))
 	}
 	response, _ := json.Marshal(payload)
 	w.Write([]byte(response))
 }
 
-func (gah *GossipResourceHandler) Get(label string, w http.ResponseWriter, r *http.Request) {
-	log.Println("GET", label)
-	gossip, err := gah.service.FindGossipByLabel(label)
+func (h *GossipResourceHandler) Get(gossipLabel string, w http.ResponseWriter, r *http.Request) {
+	log.Println("GET", gossipLabel)
+	gossip, err := h.service.FindGossipByLabel(gossipLabel)
 	if gossip == nil {
 		http.NotFound(w, r)
 		return
@@ -74,12 +88,12 @@ func (gah *GossipResourceHandler) Get(label string, w http.ResponseWriter, r *ht
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	payload := gah.buildGossipPayload(gossip)
+	payload := h.buildGossipPayload(gossip)
 	response, _ := json.Marshal(payload)
 	w.Write([]byte(response))
 }
 
-func (gah *GossipResourceHandler) Create(w http.ResponseWriter, r *http.Request) {
+func (h *GossipResourceHandler) Create(w http.ResponseWriter, r *http.Request) {
 	payload := &GossipPayload{}
 	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
 		log.Println(err)
@@ -89,15 +103,34 @@ func (gah *GossipResourceHandler) Create(w http.ResponseWriter, r *http.Request)
 
 	log.Println("POST", payload)
 	gossip, classifiers := payload.ToModel()
-	if err := gah.service.Save(gossip, classifiers); err != nil {
+	if err := h.service.Save(gossip, classifiers); err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), 500)
 		return
 	}
+
+	wasRunning := h.pool.WorkerState(WorkerID(gossip.Label)) == STARTED
+	h.pool.BuildWorker(WorkerID(gossip.Label), gossip, classifiers)
+
+	if wasRunning {
+		h.StartWorker(gossip.Label, w, r)
+	} else {
+		h.Get(gossip.Label, w, r)
+	}
 }
 
-func (gah *GossipResourceHandler) buildGossipPayload(g *Gossip) *GossipPayload {
-	classifiers, err := gah.service.FindClassifiersByGossip(g)
+func (h *GossipResourceHandler) StartWorker(gossipLabel string, w http.ResponseWriter, r *http.Request) {
+	go h.pool.StartWorker(WorkerID(gossipLabel))
+	h.Get(gossipLabel, w, r)
+}
+
+func (h *GossipResourceHandler) StopWorker(gossipLabel string, w http.ResponseWriter, r *http.Request) {
+	go h.pool.StopWorker(WorkerID(gossipLabel))
+	h.Get(gossipLabel, w, r)
+}
+
+func (h *GossipResourceHandler) buildGossipPayload(g *Gossip) *GossipPayload {
+	classifiers, err := h.service.FindClassifiersByGossip(g)
 	if err != nil {
 		log.Println(err)
 		return nil
@@ -107,5 +140,7 @@ func (gah *GossipResourceHandler) buildGossipPayload(g *Gossip) *GossipPayload {
 	for _, c := range classifiers {
 		cPayload[c.Label] = c.Patterns
 	}
-	return &GossipPayload{g.Label, g.Subjects, cPayload}
+
+	state := string(h.pool.WorkerState(WorkerID(g.Label)))
+	return &GossipPayload{g.Label, g.Subjects, cPayload, state}
 }
