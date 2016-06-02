bd3f0c865b5c5d06c53f93d30bdab8a66e47f78f
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-06-02 02:21:59 -0300

    Refactor render chart

--- webapp/bundle.js
+++ webapp/bundle.js
@@ -232,28 +232,18 @@
 	            return true;
 	        }]]);
 
-	        var that = this;
-	        console.log("renderChart");
 	        nv.addGraph(function () {
-	            var chart = nv.models.lineChart().options({
-	                duration: 300
-	            });
+	            var chart = nv.models.lineChart().options({ duration: 300 });
+	            nv.utils.windowResize(chart.update);
 
+	            chart.yAxis.axisLabel('Hits');
 	            chart.xAxis.axisLabel("Time").tickFormat(function (d) {
 	                return tickMultiFormat(new Date(d));
 	            });
 
-	            chart.yAxis.axisLabel('Hits').tickFormat(function (d) {
-	                if (d == null) {
-	                    return 'N/A';
-	                }
-	                return d3.format(',.2i')(d);
-	            });
-
 	            d3.select(this._el).datum(this.state.data).call(chart);
-	            nv.utils.windowResize(chart.update);
 
-	            that.chart = chart;
+	            this.chart = chart;
 	            return chart;
 	        }.bind(this));
 	    },
